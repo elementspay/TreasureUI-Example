5e428c4aa408b198d542723afff12829c4c71e82
Marvin Zhan <marvin.zhan@li.me>
2022-04-14 11:19:59 -0700

    remove pods first

--- Pods/Target Support Files/Pods-TreasureUIExample-TreasureUIExampleUITests/Pods-TreasureUIExample-TreasureUIExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TreasureUIExample_TreasureUIExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TreasureUIExample_TreasureUIExampleUITests
-@end
